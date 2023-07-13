0c39d38fd8e05dfbaa88dbab2ee9835c6ff1197f
nguyendinhthoi <dinhthoi2411@gmail.com>
2023-07-13 19:55:22 +0700

    Case Study 13/7/2023

--- database/case_study/sql_co_ban.sql
+++ database/case_study/sql_co_ban.sql
@@ -25,8 +25,44 @@ left join loai_khach on khach_hang.ma_loai_khach = loai_khach.ma_loai_khach
 left join hop_dong on khach_hang.ma_khach_hang = hop_dong.ma_khach_hang
 left join dich_vu on dich_vu.ma_dich_vu = hop_dong.ma_dich_vu
 left join hop_dong_chi_tiet on hop_dong_chi_tiet.ma_hop_dong = hop_dong.ma_hop_dong
-left join dich_vu_di_kem on hop_dong_chi_tiet.ma_dich_vu_di_kem = dich_vu_di_kem.ma_dich_vu_di_kem
+left join dich_vu_di_kem on hop_dong_chi_tiet.ma_dich_vu_di_kem = dich_vu_di_kem.ma_dich_vu_di_kem;
 
 -- bài 6
+select dich_vu.ma_dich_vu, dich_vu.ten_dich_vu, dich_vu.dien_tich, dich_vu.chi_phi_thue, loai_dich_vu.ten_loai_dich_vu
+from dich_vu
+left join loai_dich_vu on dich_vu.ma_loai_dich_vu = loai_dich_vu.ma_loai_dich_vu
+left join hop_dong on dich_vu.ma_dich_vu = hop_dong.ma_dich_vu
+where hop_dong.ma_dich_vu 
+not in 
+(select hop_dong.ma_dich_vu from hop_dong where (year(ngay_lam_hop_dong) = 2021) and (month(ngay_lam_hop_dong) < 4))
+group by dich_vu.ma_dich_vu;
+
+-- bài 7
+select dich_vu.ma_dich_vu, dich_vu.ten_dich_vu, dich_vu.dien_tich, dich_vu.so_nguoi_toi_da, dich_vu.chi_phi_thue, loai_dich_vu.ten_loai_dich_vu
+from dich_vu 
+join loai_dich_vu 
+on dich_vu.ma_loai_dich_vu = loai_dich_vu.ma_loai_dich_vu
+join hop_dong 
+on hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
+where hop_dong.ma_dich_vu not in 
+(select hop_dong.ma_dich_vu from hop_dong 
+where year(hop_dong.ngay_lam_hop_dong) = 2021)
+and year(hop_dong.ngay_lam_hop_dong) = 2020
+group by dich_vu.ma_dich_vu;
+
+-- bài 8
+select distinct ho_ten from khach_hang;
+
+select ho_ten from khach_hang
+group by ho_ten;
+
+select khach_hang.ho_ten from khach_hang
+union
+select khach_hang.ho_ten from khach_hang;
+
+-- bài 9
+select hop_dong.ngay_lam_hop_dong, count(ma_khach_hang) as so_khach_dat_phong
+group by hop_dong.ngay_lam_hop_dong
+
 
 
